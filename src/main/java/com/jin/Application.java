29f5efdffda3825f057f6ec3d062080f1c1afff4
hujin <flyterren@163.com>
2020-04-19 12:15:16 +0800

    spring environment config

--- src/main/java/com/jin/Application.java
+++ src/main/java/com/jin/Application.java
@@ -1,18 +1,19 @@
 package com.jin;
 
-import com.jin.service.GreetingService;
+import com.jin.config.ApplicationConfig;
 import com.jin.service.OutputService;
-import com.jin.service.TimeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        GreetingService greetingService = new GreetingService("Hello");
-        TimeService timeService = new TimeService(true);
-        OutputService outputService = new OutputService(greetingService, timeService);
+        ApplicationContext context = new AnnotationConfigApplicationContext(
+            ApplicationConfig.class);
+        OutputService outputService = context.getBean(OutputService.class);
 
         for (int i = 0; i < 5; i++) {
-            outputService.generateOutput("Jin");
+            outputService.generateOutput();
             Thread.sleep(5000);
         }
     }
