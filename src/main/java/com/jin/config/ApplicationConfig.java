096881439602b49e471f29af5a2185e933d5fa07
hujin <flyterren@163.com>
2020-04-19 13:03:18 +0800

    aop advice before

--- src/main/java/com/jin/config/ApplicationConfig.java
+++ src/main/java/com/jin/config/ApplicationConfig.java
@@ -2,11 +2,13 @@ package com.jin.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.jin")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
 }
