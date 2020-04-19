3f959e9021e684f398c603287031b2e8b35286b3
hujin <flyterren@163.com>
2020-04-19 13:06:55 +0800

    aop advice after

--- src/main/java/com/jin/aspect/LoggingAspect.java
+++ src/main/java/com/jin/aspect/LoggingAspect.java
@@ -1,9 +1,10 @@
 package com.jin.aspect;
 
 import java.util.Arrays;
+import java.util.Collection;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,8 +20,8 @@ public class LoggingAspect {
     public void executeLogging() {
     }
 
-    @Before("executeLogging()")
-    public void logMethodCall(JoinPoint joinPoint) {
+    @AfterReturning(value = "executeLogging()", returning = "returnValue")
+    public void logMethodCall(JoinPoint joinPoint, Object returnValue) {
         StringBuilder message = new StringBuilder("Method: ");
         message.append(joinPoint.getSignature().getName());
         Object[] args = joinPoint.getArgs();
@@ -30,6 +31,12 @@ public class LoggingAspect {
                 message.append(arg).append(" | ");
             });
             message.append("]");
+        }
+        if (returnValue instanceof Collection) {
+            message.append(", returning: ").append(((Collection) returnValue).size())
+                .append(" instance(s)");
+        } else {
+            message.append(", returning: ").append(returnValue.toString());
         }
         LOGGER.info(message.toString());
     }
